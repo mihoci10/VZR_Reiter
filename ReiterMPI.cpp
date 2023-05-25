b42f2afeea322334d42e3d42ad281b55db7adca9
mihoci10 <miha.krajnc8@gmail.com>
2023-05-25 20:04:02 +0200

    Fix execution of MPI paralelization

--- ReiterMPI.cpp
+++ ReiterMPI.cpp
@@ -3,6 +3,9 @@
 #include <chrono>
 #include <mpi.h>
 
+#define min(a,b) (((a)<(b))?(a):(b))
+#define max(a,b) (((a)>(b))?(a):(b))
+
 double ReiterMPI::RunSimulation(float alpha, float beta, float gamma)
 {
     auto start = std::chrono::high_resolution_clock::now();
@@ -15,125 +18,158 @@ double ReiterMPI::RunSimulation(float alpha, float beta, float gamma)
     return (duration.count() * 1e-6);
 }
 
+void GetNeighbourCellIdsMPI(size_t global_cell_id, int* outIdArray, int width)
+{
+    size_t j = global_cell_id % width;
+
+    int nOff;
+    if (j%2 == 0)
+        nOff = -1;
+    else
+        nOff = 0;
+        
+    outIdArray[0] = width * (-1);
+    outIdArray[1] = width * (nOff) - 1;
+    outIdArray[2] = width * (nOff+1) - 1;
+    outIdArray[3] = width * (nOff) + 1;
+    outIdArray[4] = width * (nOff+1) + 1;
+    outIdArray[5] = width * (1);
+}
+
 void ReiterMPI::Simulation(float alpha, float beta, float gamma){
 
-    // Number of processes and rank
-    int n_proc;
+    int rank, n_proc;
 	MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
-	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    auto curData = CreateGrid(beta);
-    auto prevData = CreateGrid(beta);
+    std::shared_ptr<float> curData;
+    //if (rank == 0) 
+        curData = CreateGrid(beta);
     
-    // Array describing how many elements to send to each process
-	int* sendcounts = (int*)malloc(sizeof(int) * n_proc);
+    int N = m_Width * m_Height;
+	int block_size = N / n_proc + 1;
 
-    // Array describing the displacements where each segment begins
-	int* displacements = (int*)malloc(sizeof(int) * n_proc);
+	int* rcv_buf_sizes = new int[n_proc];
+	int* rcv_buf_displ = new int[n_proc];
 
-    // Block size
-	int block_size = (m_Width * m_Height) / n_proc;
+	int* snd_buf_sizes = new int[n_proc];
+	int* snd_buf_displ = new int[n_proc];
+    
+	int snd_sum = 0;
+    for (int i = 0; i < n_proc; i++)
+	{
+		int diff = min(block_size, N - snd_sum);
+		snd_buf_sizes[i] = diff;
+		snd_buf_displ[i] = snd_sum;
+		snd_sum += diff;
 
-    // Sum of counts, used to calculate displacements
-	int sum = 0;
-
-    // Elements remaining after division among processes
-	int remainder = (m_Width * m_Height) % n_proc;
-
-    // Max size of receiving buffer
-	int max_receive_buffer_size = 0;
-
-    // Calculate send counts and displacements
-	for(int i=0; i<n_proc; i++){
-		sendcounts[i] = block_size;
-		if(remainder > 0){
-			sendcounts[i]++;
-			remainder--;
-		}
-
-		displacements[i] = sum;
-		sum += sendcounts[i];
-
-		if(max_receive_buffer_size < sendcounts[i])
-			max_receive_buffer_size = sendcounts[i];
+        rcv_buf_sizes[i] = snd_buf_sizes[i] + min(m_Width + 1, snd_buf_displ[i]) + min(m_Width + 1, N - snd_buf_displ[i] - snd_buf_sizes[i]);
+		rcv_buf_displ[i] = snd_buf_displ[i] - min(m_Width + 1, snd_buf_displ[i]);
 	}
 
-    // Buffer where the received data should be stored
-	float* receive_buffer = (float*)malloc(max_receive_buffer_size * sizeof(float));
+    int start_cell_id = snd_buf_displ[rank];
+    int stop_cell_id = start_cell_id + snd_buf_sizes[rank];
+
+    int rcv_buf_start_pad = min(m_Width + 1, start_cell_id);
+    int rcv_buf_stop_pad = min(m_Width + 1, N - stop_cell_id);
+
+	int rcv_buf_size = snd_buf_sizes[rank];
+    rcv_buf_size += rcv_buf_start_pad;
+    rcv_buf_size += rcv_buf_stop_pad;
+    int snd_buf_size = snd_buf_sizes[rank];
+
+	auto rcv_buf = std::shared_ptr<float>((float*)malloc(rcv_buf_size * sizeof(float)), free);
+    auto snd_buf = std::shared_ptr<float>((float*)malloc(snd_buf_size * sizeof(float)), free);
+
+    auto idArray = std::shared_ptr<int>((int*)malloc(6 * sizeof(int)), free);
+    auto idArrayS = std::shared_ptr<int>((int*)malloc(6 * sizeof(int)), free);
 
     size_t iter = 0;
-    auto idArray = std::shared_ptr<size_t>((size_t*)malloc(6 * sizeof(size_t)), free);
-    float* updatedData = (float*)malloc(max_receive_buffer_size * sizeof(float));
 
-    // Compute
     while(iter <= MAX_ITER){
 
-        // Scatter curData
-	    MPI_Scatterv(curData.get(), sendcounts, displacements, MPI_FLOAT, receive_buffer, max_receive_buffer_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	    MPI_Scatterv(curData.get(), rcv_buf_sizes, rcv_buf_displ, MPI_FLOAT, rcv_buf.get(), rcv_buf_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-        for(int i=0; i < sendcounts[rank]; i++){
+        for(int i=0; i < snd_buf_size; i++){
 
-            int cellId = displacements[rank] + i;
-            GetNeighbourCellIds(cellId, idArray.get());
-            float sum = 0;
-            for(int k = 0; k < 6; k++){
-                int id = idArray.get()[k];
-                if(!CheckReceptiveCell(prevData.get(), id))
-                    sum += prevData.get()[id];
+            int global_cell_id = start_cell_id + i;
+            int global_j = global_cell_id % m_Width;
+            int global_i = (global_cell_id - global_j) / m_Width;
+
+            if(global_i == 0 || global_j == 0 || m_Height - global_i == 1 || m_Width - global_j == 1){
+                snd_buf.get()[i] = beta;
+                continue;
             }
 
-            float cellR = (CheckReceptiveCell(prevData.get(), cellId) ? 1.0 : 0.0);
-            float cellU = (cellR == 0.0 ? prevData.get()[cellId] : 0.0);
+            GetNeighbourCellIdsMPI(global_cell_id, idArray.get(), m_Width);
+            float sum = 0;
+            bool receptive = rcv_buf.get()[rcv_buf_start_pad + i] >= 1;
+            for(int k = 0; k < 6; k++){
+                int id = idArray.get()[k] + rcv_buf_start_pad + i;
+                bool receptiveS = false;
+                if(rcv_buf.get()[id] >= 1)
+                    receptiveS = true;
+                else{
+                    GetNeighbourCellIdsMPI(global_cell_id + idArray.get()[k], idArrayS.get(), m_Width);
+                    for (int v = 0; v < 6; v++)
+                    {
+                        if(id + idArrayS.get()[v] >= 0 && id + idArrayS.get()[v] < rcv_buf_size)
+                            if(rcv_buf.get()[id + idArrayS.get()[v]] >= 1)
+                                receptiveS = true;
+                        if(receptiveS)
+                            break;
+                    }
+                }
+                
+                if(!receptiveS)
+                    sum += rcv_buf.get()[id];
+                if(rcv_buf.get()[id] >= 1)
+                    receptive = true;
+            }
 
-            curData.get()[cellId] = prevData.get()[cellId] +  (alpha / 2.0) * ((sum / 6.0) - cellU) + (gamma * cellR);
+            float cellR = (receptive ? 1.0 : 0.0);
+            float cellU = (cellR == 0.0 ? rcv_buf.get()[rcv_buf_start_pad + i] : 0.0);
 
-            // Store the updated cell data in the updatedData buffer
-            updatedData[i] = curData.get()[cellId];
-
+            snd_buf.get()[i] = rcv_buf.get()[rcv_buf_start_pad + i] +  (alpha / 2.0) * ((sum / 6.0) - cellU) + (gamma * cellR);
         }
 
-        if(m_DebugFreq == DebugFreq::EveryIter)
+        MPI_Gatherv(snd_buf.get(), snd_buf_size, MPI_FLOAT, curData.get(), snd_buf_sizes, snd_buf_displ, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+        if(rank == 0 && m_DebugFreq == DebugFreq::EveryIter)
             LogState(curData.get(), iter);
 
-        // Gather the updated cell data from all nodes
-        MPI_Gatherv(updatedData, sendcounts[rank], MPI_FLOAT, curData.get(), sendcounts, displacements, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-        curData.swap(prevData);
         iter++;
     }
 
-    if(rank == 0){
-        printf("Simulation took %ld iterations\n", iter);
-        if(m_DebugFreq == DebugFreq::Last)
-        LogState(prevData.get(), iter);
-    }
+    if(rank == 0 && m_DebugFreq == DebugFreq::Last)
+        LogState(curData.get(), iter);
 
-    // Clean up
-    free(sendcounts);
-    free(displacements);
-    free(receive_buffer);
-
+    delete[] snd_buf_sizes;
+    delete[] snd_buf_displ;
 }
 
 int main(int argc, char** argv){
 
+    int width, height;
+    float alpha, beta, gamma;
+
+    if (!ReiterSimulation::ParseInputParams(argc, argv, &width, &height, &alpha, &beta, &gamma))
+    {
+        printf("Correct usage should be: %s <width> <height> <alpha> <beta> <gamma>\n", argv[0]);
+        return -1;
+    }
+
     MPI_Init(&argc, &argv);
 
-    // Rank
-	int rank;
+	int rank, n_proc;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
 
-    int w = 500;
-    int h = 500;
-    float alpha = 1;
-    float beta = 0.6;
-    float gamma = 0.01;
-
-    ReiterMPI model(w, h);
+    ReiterMPI model(width, height);
 
     if(rank == 0){
         auto dur = model.RunSimulation(alpha, beta, gamma);
+        printf("Ran on %d nodes\n", n_proc);
         printf("Execution took %lf seconds\n", dur);
     }
     else{
--- run_mpi.sh
+++ run_mpi.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-bash build.sh && srun --reservation=fri --mpi=pmix -n 4 -N 1 out/ReiterMPI
+
+bash build.sh && srun --reservation=fri --mpi=pmix -n 2 -N 1 out/ReiterMPI 100 100 1 0.5 0.01
